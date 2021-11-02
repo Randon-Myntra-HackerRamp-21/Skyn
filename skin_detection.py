3c3619acd0ca5dfb4b991a6d6c7fb923fffdf034
Sylamsh <amrutasrisvara@gmail.com>
2021-11-03 00:46:05 +0530

    skin tone-combinational thresholding implemented

--- skin_detection.py
+++ skin_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 # ---- START FUNCTIONS ----#
@@ -18,14 +19,36 @@ def display_image(image, name):
 
 # segment using otsu binarization and thresholding
 def segment_otsu(image_grayscale, img_BGR):
-    threshold_value, threshold_image_otsu = cv2.threshold(
+    Totsu, threshold_image_otsu = cv2.threshold(
         image_grayscale, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    threshold_value, threshold_image_max = cv2.threshold(
-        image_grayscale, 0, np.max(image_grayscale), cv2.THRESH_BINARY_INV)
-    display_image(threshold_image_otsu, "otsu")
-    display_image(threshold_image_max, "max")
+    print("Totsu : ", Totsu)
+    # display_image(threshold_image_otsu, "otsu")
 
-    masked_img = cv2.bitwise_and(img_BGR, img_BGR, mask=threshold_image_otsu)
+    histogram, bin_edges = np.histogram(image_grayscale.ravel(), 256, [0, 256])
+    Tmax = np.where(histogram[:] == max(histogram[:]))[0][0]
+    print("Tmax :", Tmax)
+
+    Tfinal = round((Tmax + Totsu)/2) if Tmax > 10 else round((Tmax + Totsu)/4)
+
+    # print("Histogram : \n", histogram)
+    # print("bin_edges : \n", bin_edges)
+    plt.figure()
+    plt.title("Image Histogram")
+    plt.xlabel("pixel value")
+    plt.ylabel("pixel frequency")
+    plt.xlim([0, 256])
+    plt.plot(bin_edges[0:-1], histogram)  # <- or here
+    plt.axvline(x=Tmax, label="Tmax", color='red', linestyle="--")
+    plt.axvline(x=Totsu, label="Totsu", color='green', linestyle="--")
+    plt.axvline(x=Tfinal, label="Tfinal", color='yellow', linestyle="-")
+    plt.legend()
+    plt.show()
+
+    threshold_type = cv2.THRESH_BINARY if Tmax < 200 else cv2.THRESH_BINARY_INV
+    Totsu, threshold_image = cv2.threshold(
+        image_grayscale, Tfinal, 255, threshold_type)
+    display_image(threshold_image, "threshold_image")
+    masked_img = cv2.bitwise_and(img_BGR, img_BGR, mask=threshold_image)
     display_image(masked_img, "masked_img")
     # threshold_image_binary = 1 - threshold_image_otsu/255
     # cv2.imshow("img_binary", threshold_image_binary)
@@ -43,8 +66,9 @@ def segment_otsu(image_grayscale, img_BGR):
 
 
 # read in image into openCV
-image_path = "images\Optimized-050_AppleiPhoneXSMax_DxOMark_Selfie-e1557175226410.jpg"
+image_path = "D:\My Stuff\Pictures\phonephotos\\20200604_070026.jpg"
 img_BGR = cv2.imread(image_path, 3)
+img_BGR = cv2.resize(img_BGR, (375, 500))
 
 # Grayscle and Thresholding and HSV & YCrCb color space conversions
 img_grayscale = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2GRAY)
@@ -67,10 +91,11 @@ img_skin_predict = img_grayscale
 for i in range(height):
     for j in range(width):
         if((img_HSV.item(i, j, 0) <= 170) and (140 <= img_YCrCb.item(i, j, 1) <= 170) and (90 <= img_YCrCb.item(i, j, 2) <= 120)):
-            img_skin_predict[i, j] = 255
+            img_skin_predict[i, j] = 1
         else:
             img_skin_predict[i, j] = 0
-display_image(img_skin_predict, "skin prediction")
+display_image(cv2.bitwise_and(img_BGR, img_BGR,
+              mask=img_skin_predict), "skin prediction")
 
 # Contruction the dataframe for K-means clustering
 dframe = pd.DataFrame()
@@ -79,7 +104,6 @@ dframe['H'] = img_HSV.reshape([-1, 3])[:, 0]
 # Getting the y-x coordintated
 gray = cv2.cvtColor(img_no_background, cv2.COLOR_BGR2GRAY)
 yx_coords = np.column_stack(np.where(gray >= 0))
-# dframe['YX'] = tuple(yx_coords[:])
 dframe['Y'] = yx_coords[:, 0]
 dframe['X'] = yx_coords[:, 1]
 
