8fd89e20649d88b2199fb69fa5a40c9551c3c38d
Sylamsh <amrutasrisvara@gmail.com>
2021-11-06 19:13:11 +0530

    Webcam configured and basic styling added

---
+++ frontend/src/views/imageInput.jsx
@@ -0,0 +1,20 @@
+import WebcamCapture from './Components/webCam'
+import React from 'react'
+
+// MUI
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+
+function ImageInput() {
+    return (
+        <>
+            <Container maxWidth="xs" sx={{padding: 0}}>
+                <Grid container justify="center">
+                    <WebcamCapture />
+                </Grid>
+            </Container>
+        </>
+    )
+}
+
+export default ImageInput
