0dbe7be247f3d96e3b005773649a237f579fc7c9
guilefoylegaurav <grvb1515@gmail.com>
2021-11-07 15:46:01 +0530

    Added controller to form. #7

--- frontend/src/views/Form.jsx
+++ frontend/src/views/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
+import React, { useState, setState, useRef, useCallback, useEffect } from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
@@ -32,7 +32,18 @@ import Button from '@mui/material/Button';
 const Form = () => {
 
 
-
+    const [features, setFeatures] = useState({normal:false, dry:false, oily:false, combination:false, 
+    acne:false, sensitive:false, fineLines:false, wrinkles:false, 
+    "redness":false, dull:false, pore:false, pigmentation:false, 
+    blackheads:false, whiteheads:false, blemishes:false, darkCircles:false, 
+    eyeBags:false, darkSpots:false});
+    const handleChange = (event) => {
+    setFeatures({
+      ...features,
+      [event.target.name]: event.target.checked,
+    });
+    console.log(features)
+  };
     return (<Container fixed sx={{ p: 10, textAlign:"center" }}>
         
         {/* <FormControl component="fieldset" > */}
@@ -68,65 +79,61 @@ const Form = () => {
             <FormLabel component="legend">Other concerns</FormLabel>
             <Grid container spacing={2}>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.acne} onChange={handleChange} name = "acne" />} label="acne" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.sensitive} onChange={handleChange}  name = "sensitive"/>} label="sensitive" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.fineLines} onChange={handleChange} name = "fineLines"/>} label="fine Lines" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.wrinkles} onChange={handleChange} name="wrinkles" />} label="wrinkles" />
                 </Grid>
 
 
 
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.redness} onChange={handleChange} name = "redness" />} label="redness" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.dull} onChange={handleChange} name = "dullness" />} label="dullness" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.pore} onChange={handleChange} name="pore"/>} label="large pores" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.pigmentation} onChange={handleChange} name="pigmentation" />} label="pigmentation" />
                 </Grid>
 
 
 
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.blackheads} onChange={handleChange} name = "blackheads" />} label="blackheads" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.whiteheads} onChange={handleChange} name="whiteheads" />} label="whiteheads" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.blemishes} onChange={handleChange} name = "blemishes" />} label="blemishes" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.darkCircles} onChange={handleChange} name="darkCircles" />} label="dark circles" />
                 </Grid>
 
 
 
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.eyeBags} onChange={handleChange} name = "eyeBags"/>} label="eye bags" />
                 </Grid>
                 <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                    <FormControlLabel control={<Checkbox checked={features.darkSpots} onChange={handleChange} name = "darkSpots" />} label="dark spots" />
                 </Grid>
-                <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-                </Grid>
-                <Grid item xs={3}>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-                </Grid>
-
-                <Button variant="outlined" sx = {{ m:"auto"}}>Outlined</Button>
+                
+                
+                
             </Grid>
+            <Button variant="outlined" sx = {{ m:"auto"}}>Outlined</Button>
 
 
 
